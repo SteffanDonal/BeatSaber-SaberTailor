78d1a502b2ada41725fdd55f77847447d4d25e34
Steffan Donal <steffan@sdonalcreative.uk>
2018-08-21 11:34:55 +0200

    Plugin base!

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using IllusionPlugin;
+
+namespace SaberTweaks
+{
+    public class Plugin : IPlugin
+    {
+        public const string Name = "SaberTweaks";
+        public const string Version = "0.0.1";
+
+        string IPlugin.Name => Name;
+        string IPlugin.Version => Version;
+
+        public void OnApplicationStart() { }
+        public void OnApplicationQuit() { }
+
+        public void OnLevelWasLoaded(int level) { }
+        public void OnLevelWasInitialized(int level) { }
+
+        public void OnUpdate() { }
+
+        public void OnFixedUpdate() { }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SaberTweaks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SaberTweaks")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a2d66447-9928-4292-9a82-87abbaa401ff")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
