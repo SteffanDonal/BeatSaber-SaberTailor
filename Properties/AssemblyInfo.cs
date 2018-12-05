90f39b2dda2477af575df7c170ffcc7708bd1193
Shadnix <shadnix@arcor.de>
2018-12-05 01:19:01 +0100

    Cleanup for pull request

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SaberTailor")]
-[assembly: AssemblyCopyright("Copyright © Steffan Donal 2018, Shadnix 2018")]
+[assembly: AssemblyCopyright("Copyright © Steffan Donal 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a2d66447-9928-4292-9a82-87abbaa401ff")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
